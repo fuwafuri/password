6a8e6936bb793a30eb040154fbb8171f6f369fae
fuwafuri <118356714+fuwafuri@users.noreply.github.com>
2024-11-17 20:39:52 +0900

    Add files via upload

---
+++ yajyu.js
@@ -0,0 +1,26 @@
+let pass = prompt("合言葉を入力してください(普通にキモイ)");
+switch (pass) {
+    case "まんこ":
+        window.open("https://fmsabu.github.io/manko/manko.html");
+        break;
+
+    case "ちんこ":
+        window.open("https://fmsabu.github.io/tinko/tinko.html");
+        break;
+
+    case "おっぱい":
+        window.open("");
+        break;
+
+    case "野獣先輩":
+        window.open("");
+        break;
+    
+    case "エロ漫画":
+        window.open("");
+        break;
+
+    default:
+        alert("ページを更新してやり直してください");
+        break;
+}
